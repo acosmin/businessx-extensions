1df7e793f85a78b6545862b05bd26d0cde6fd248
acosmin <acosmin.com@gmail.com>
2016-09-20 10:43:36 +0300

    Added backwards compatibility + changed textdomains
    For functions moved from theme into plugin

--- inc/functions/helpers.php
+++ inc/functions/helpers.php
@@ -3,6 +3,24 @@
  *  Helper Functions
 /* ------------------------------------------------------------------------- */
 
+
+
+/*  Check theme version
+/* ------------------------------------ */
+if( ! function_exists( 'businessx_extensions_ck_theme_v' ) ) {
+    function businessx_extensions_ck_theme_v( $version ) {
+        $theme_name = apply_filters( 'businessx_extensions___get_theme_name', 'businessx');
+        $theme_ver = wp_get_theme( $theme_name )->get('Version');
+        if( version_compare( $theme_ver, $version, '>' ) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
 /*  Check if Jetpack specific module
  *  is enabled
 /* ------------------------------------ */
@@ -36,18 +54,19 @@ add_action( 'admin_notices', 'businessx_extensions_jp_ck_mobile_theme', 0 );
 
 
 
+if( businessx_extensions_ck_theme_v( '1.0.3' ) ) : // Backwards compatibility
 /*  Hero buttons output
 /* ------------------------------------ */
 if( ! function_exists( 'businessx_hero_btns_output' ) ) {
     function businessx_hero_btns_output() {
     	$type = get_theme_mod( 'hero_section_btns', apply_filters( 'businessx_hero___btns_type_default', 'btns-2-def-op' ) );
-    	$btn_1_text = get_theme_mod( 'hero_section_1st_btn', __( 'Call to Action', 'businessx' ) );
+    	$btn_1_text = get_theme_mod( 'hero_section_1st_btn', __( 'Call to Action', 'businessx-extensions' ) );
     	$btn_1_link = get_theme_mod( 'hero_section_1st_btn_link', '#' );
     	$btn_1_target = get_theme_mod( 'hero_section_1st_btn_target', false ) ? '_blank' : '_self';
-    	$btn_2_text = get_theme_mod( 'hero_section_2nd_btn', __( 'Call to Action', 'businessx' ) );
+    	$btn_2_text = get_theme_mod( 'hero_section_2nd_btn', __( 'Call to Action', 'businessx-extensions' ) );
     	$btn_2_link = get_theme_mod( 'hero_section_2nd_btn_link', '#' );
     	$btn_2_target = get_theme_mod( 'hero_section_2nd_btn_target', false ) ? '_blank' : '_self';
-    	$btns_between = get_theme_mod( 'hero_section_btns_or', __( 'Or', 'businessx' ) );
+    	$btns_between = get_theme_mod( 'hero_section_btns_or', __( 'Or', 'businessx-extensions' ) );
     	$output = '';
 
     	switch( $type ) {
@@ -118,10 +137,10 @@ if( ! function_exists( 'businessx_hero_btns_output' ) ) {
 /* ------------------------------------ */
 if( ! function_exists( 'businessx_slider_btns_output' ) ) {
     function businessx_slider_btns_output( $type = 'btns-2-def-op', $btns_between = ' ', $btn_1_text = '', $btn_1_link = '', $btn_1_target = false, $btn_2_text = '', $btn_2_link = '', $btn_2_target = false ) {
-    	$btn_1_text 	= ! empty( $btn_1_text ) ? $btn_1_text : esc_html__( 'Call to Action #1', 'businessx' );
+    	$btn_1_text 	= ! empty( $btn_1_text ) ? $btn_1_text : esc_html__( 'Call to Action #1', 'businessx-extensions' );
     	$btn_1_link 	= ! empty( $btn_1_link ) ? $btn_1_link : '#';
     	$btn_1_target	= $btn_1_target ? '_blank' : '_self';
-    	$btn_2_text 	= ! empty( $btn_2_text ) ? $btn_2_text : esc_html__( 'Call to Action #2', 'businessx' );
+    	$btn_2_text 	= ! empty( $btn_2_text ) ? $btn_2_text : esc_html__( 'Call to Action #2', 'businessx-extensions' );
     	$btn_2_link 	= ! empty( $btn_2_link ) ? $btn_2_link : '#';
     	$btn_2_target	= $btn_2_target ? '_blank' : '_self';
     	$output = '';
@@ -198,42 +217,42 @@ if( ! function_exists( 'businessx_slider_btns_select' ) ) {
     		$options = apply_filters( 'businessx_slider_btns___select', $options = array(
     				array(
     					'value' 	=> 'btns-1-default',
-    					'title'		=> esc_html__( 'One - Default', 'businessx' ),
+    					'title'		=> esc_html__( 'One - Default', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-1-opaque',
-    					'title'		=> esc_html__( 'One - Opaque', 'businessx' ),
+    					'title'		=> esc_html__( 'One - Opaque', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-1-l-default',
-    					'title'		=> esc_html__( 'One Large - Default', 'businessx' ),
+    					'title'		=> esc_html__( 'One Large - Default', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-1-l-opaque',
-    					'title'		=> esc_html__( 'One Large - Opaque', 'businessx' ),
+    					'title'		=> esc_html__( 'One Large - Opaque', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-2-default',
-    					'title'		=> esc_html__( 'Two - Default', 'businessx' ),
+    					'title'		=> esc_html__( 'Two - Default', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-2-opaque',
-    					'title'		=> esc_html__( 'Two - Opaque', 'businessx' ),
+    					'title'		=> esc_html__( 'Two - Opaque', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-2-def-op',
-    					'title'		=> esc_html__( 'Two - Default + Opaque', 'businessx' ),
+    					'title'		=> esc_html__( 'Two - Default + Opaque', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     				array(
     					'value' 	=> 'btns-2-op-def',
-    					'title'		=> esc_html__( 'Two - Opaque + Default', 'businessx' ),
+    					'title'		=> esc_html__( 'Two - Opaque + Default', 'businessx-extensions' ),
     					'disabled'	=> false
     				),
     		) );
@@ -246,3 +265,4 @@ if( ! function_exists( 'businessx_slider_btns_select' ) ) {
     	return $options;
     }
 }
+endif; // Backwards compatibility END
